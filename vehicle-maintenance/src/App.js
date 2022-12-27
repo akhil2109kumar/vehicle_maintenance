43403aef7c118b78a80736f1ce5d1973742f2126
akhil2109kumar <akhil2109kumar@gmail.com>
2022-12-27 17:15:20 +0530

    Initial commit -frontend

---
+++ vehicle-maintenance/src/App.js
@@ -0,0 +1,13 @@
+import LandingPage from './components/landingPage';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/style.css'
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
